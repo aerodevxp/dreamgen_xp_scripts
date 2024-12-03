704c82d9fd4365ef471059a800892b8cbdc4dd71
Raphael Beauregard <rnga3672@gmail.com>
2024-12-03 13:00:04 -0500

    Update quickgenshortcuts.user.js
    added support for RP and Sandbox

--- quickgenshortcuts.user.js
+++ quickgenshortcuts.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @description  Simulate button clicks using keyboard shortcuts (Ctrl+Enter and Alt+R mainly)
 // @author       aeroraphxp
-// @match        https://dreamgen.com/app/stories/v2/*
+// @match        https://dreamgen.com/app/*
 // @grant        none
 // ==/UserScript==
 
@@ -14,6 +14,7 @@
     function simulateClick(selector) {
         const button = document.querySelector(selector);
         if (button) {
+            console.log("Simulating Click!");
             button.click();
         }
     }
@@ -22,12 +23,21 @@
     document.addEventListener('keydown', function(event) {
         // Ctrl + Enter
         if (event.ctrlKey && event.key === 'Enter') {
+            //Story Button
             simulateClick("body > div.flex.flex-col.h-full > div.h-full.flex.flex-col.lg\\:pl-48 > div.page-content-wrapper > div > div > div.flex.h-full.w-full.max-w-5xl > div > div.focus-within\\:border-muted-foreground\\/50.flex.flex-row.items-center.justify-between.webkit-padding-right-12px > div.flex.flex-row.gap-x-3 > button.inline-flex.items-center.justify-center.whitespace-nowrap.text-sm.font-medium.transition-colors.focus-visible\\:outline-none.focus-visible\\:ring-1.focus-visible\\:ring-ring.disabled\\:pointer-events-none.disabled\\:opacity-50.bg-secondary.text-secondary-foreground.shadow-sm.hover\\:bg-secondary\\/80.h-9.px-4.py-2.rounded-md");
+            //RP Button
+            const RPCONTINUE = "#interaction-editor-R2htttttsrltsrla > div.flex.flex-col.w-full > div.flex.flex-col.gap-y-2.pb-0\\.5 > div.flex.flex-row.items-center.justify-between.gap-x-2 > div.flex.flex-row.items-center.gap-x-2 > button"
+            simulateClick(RPCONTINUE)
+            //Sandbox
+            simulateClick("body > div.flex.flex-col.h-full > div.h-full.flex.flex-col.lg\\:pl-48 > div.page-content-wrapper > div > div > div.flex.flex-col.h-full.w-full.lg\\:max-w-screen-md.overflow-hidden > div.flex.flex-row.justify-between.mt-2.px-1.webkit-padding-right-12px > div > button")
         }
 
         // Alt + R
         if (event.altKey && event.key === 'r') {
+            //Story
             simulateClick("body > div.flex.flex-col.h-full > div.h-full.flex.flex-col.lg\\:pl-48 > div.page-content-wrapper > div > div > div.flex.h-full.w-full.max-w-5xl > div > div.focus-within\\:border-muted-foreground\\/50.flex.flex-row.items-center.justify-between.webkit-padding-right-12px > div.flex.flex-row.gap-x-3 > button.inline-flex.items-center.justify-center.whitespace-nowrap.text-sm.font-medium.transition-colors.focus-visible\\:outline-none.focus-visible\\:ring-1.focus-visible\\:ring-ring.disabled\\:pointer-events-none.disabled\\:opacity-50.hover\\:bg-accent.hover\\:text-accent-foreground.h-9.w-9.shrink-0.rounded-md");
+            //RP
+            simulateClick("body > div.flex.flex-col.h-full > div.h-full.flex.flex-col.lg\\:pl-48 > div.page-content-wrapper > div > div > div.flex.flex-col.h-full.justify-between.w-full.lg\\:max-w-7xl.overflow-hidden.mx-auto > div.flex.flex-col.gap-y-2.overflow-auto.custom-scrollbar.dark.pr-0\\.5.pb-2 > div.pl-10.flex.flex-row.justify-between.w-full.leading-none > div > button")
         }
     });
 })();
