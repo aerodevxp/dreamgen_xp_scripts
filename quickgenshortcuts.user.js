70c0e0941d460193a4b5cd381d5e6d930d151ccd
Raphael Beauregard <rnga3672@gmail.com>
2024-12-10 20:55:33 -0500

    Update quickgenshortcuts.user.js - fixed with new ui layout

--- quickgenshortcuts.user.js
+++ quickgenshortcuts.user.js
@@ -24,9 +24,9 @@
         // Ctrl + Enter
         if (event.ctrlKey && event.key === 'Enter') {
             //Story Button
-            simulateClick("body > div.flex.flex-col.h-full > div.h-full.flex.flex-col.lg\\:pl-48 > div.page-content-wrapper > div > div > div.flex.h-full.w-full.max-w-5xl > div > div.focus-within\\:border-muted-foreground\\/50.flex.flex-row.items-center.justify-between.webkit-padding-right-12px > div.flex.flex-row.gap-x-3 > button.inline-flex.items-center.justify-center.whitespace-nowrap.text-sm.font-medium.transition-colors.focus-visible\\:outline-none.focus-visible\\:ring-1.focus-visible\\:ring-ring.disabled\\:pointer-events-none.disabled\\:opacity-50.bg-secondary.text-secondary-foreground.shadow-sm.hover\\:bg-secondary\\/80.h-9.px-4.py-2.rounded-md");
+            simulateClick("button.px-4:nth-child(2)");
             //RP Button
-            const RPCONTINUE = "#interaction-editor-R2htttttsrltsrla > div.flex.flex-col.w-full > div.flex.flex-col.gap-y-2.pb-0\\.5 > div.flex.flex-row.items-center.justify-between.gap-x-2 > div.flex.flex-row.items-center.gap-x-2 > button"
+            const RPCONTINUE = "button.bg-primary"
             simulateClick(RPCONTINUE)
             //Sandbox
             simulateClick("body > div.flex.flex-col.h-full > div.h-full.flex.flex-col.lg\\:pl-48 > div.page-content-wrapper > div > div > div.flex.flex-col.h-full.w-full.lg\\:max-w-screen-md.overflow-hidden > div.flex.flex-row.justify-between.mt-2.px-1.webkit-padding-right-12px > div > button")
@@ -35,9 +35,9 @@
         // Alt + R
         if (event.altKey && event.key === 'r') {
             //Story
-            simulateClick("body > div.flex.flex-col.h-full > div.h-full.flex.flex-col.lg\\:pl-48 > div.page-content-wrapper > div > div > div.flex.h-full.w-full.max-w-5xl > div > div.focus-within\\:border-muted-foreground\\/50.flex.flex-row.items-center.justify-between.webkit-padding-right-12px > div.flex.flex-row.gap-x-3 > button.inline-flex.items-center.justify-center.whitespace-nowrap.text-sm.font-medium.transition-colors.focus-visible\\:outline-none.focus-visible\\:ring-1.focus-visible\\:ring-ring.disabled\\:pointer-events-none.disabled\\:opacity-50.hover\\:bg-accent.hover\\:text-accent-foreground.h-9.w-9.shrink-0.rounded-md");
+            simulateClick("button.w-9:nth-child(1)");
             //RP
-            simulateClick("body > div.flex.flex-col.h-full > div.h-full.flex.flex-col.lg\\:pl-48 > div.page-content-wrapper > div > div > div.flex.flex-col.h-full.justify-between.w-full.lg\\:max-w-7xl.overflow-hidden.mx-auto > div.flex.flex-col.gap-y-2.overflow-auto.custom-scrollbar.dark.pr-0\\.5.pb-2 > div.pl-10.flex.flex-row.justify-between.w-full.leading-none > div > button")
+            simulateClick("html.dark body.__variable_d65c78.__variable_86777a.__variable_9736f3.font-sans div.flex.flex-col.h-full div.h-full.flex.flex-col.lg:pl-48 div.page-content-wrapper div.page-content.w-full.max-w-none div.flex.flex-row.overflow-hidden.h-full.gap-x-2.justify-between div.flex.flex-col.h-full.justify-between.w-full.lg:max-w-7xl.overflow-hidden.mx-auto div.flex.flex-col.gap-y-2.overflow-auto.custom-scrollbar.dark.pr-0.5.pb-2 div.pl-10.flex.flex-row.justify-between.w-full.leading-none div.mx-auto button.inline-flex.items-center.justify-center.whitespace-nowrap.text-sm.font-medium.transition-colors.focus-visible:outline-none.focus-visible:ring-1.focus-visible:ring-ring.disabled:pointer-events-none.disabled:opacity-50.hover:bg-accent.hover:text-accent-foreground.shrink-0.rounded-md.h-5.w-10")
         }
     });
 })();
